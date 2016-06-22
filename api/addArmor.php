52d1f9ea0eb9ae205f176349496013f8feba2d4c
jvallier <jvallier@ymail.com>
2016-06-22 23:35:28 +0000

    Add files via upload

--- api/addArmor.php
+++ api/addArmor.php
@@ -5,14 +5,18 @@ if(!isset($_POST['type'])){
 	die(json_encode(array('error'=>true,'message'=>'Missing Armor Type')));
 }
 /*Check to see if there is a name and a value, and that the value is > 0*/
-if(!isset($_POST['name']))
-{die(json_encode(array('error'=>true,'message'=>'Missing Armor Name'));}
-if(!isset($_POST['value']) && $_POST['value'] > 0)
-{die(json_encode(array('error'=>true,'message'=>'Missing Armor Value or Value is not greater than 0'));}
+if(!isset($_POST['name'])){
+	die(json_encode(array('error'=>true,'message'=>'Missing Armor Name')));
+
+/*Needed to change the && used previously, made the mistake of reading AND in readme and adding it into code*/
+if(!isset($_POST['value']) || $_POST['value'] < 0){
+	die(json_encode(array('error'=>true,'message'=>'Missing Armor Value or Value is not greater than 0')));
+}
 
 /*If name is set check to see that there are no armors of the same name*/
-if(Storage::getInventory($_POST['name']))
-{die(json_encode(array('error'=>true,'message'=>'Armor has same name as armor that already exists.'));}
+if(Storage::getInventory($_POST['name'])){
+	die(json_encode(array('error'=>true,'message'=>'Armor has same name as armor that already exists.')));
+}
 	
 $armor = Armor::createArmor($_POST['type']);
 $armor->setName($_POST['name'])->setValue($_POST['value']);
